e7849ddbc5edc92d6417b71d1c6ad11b6fa386d4
Yevhen Melnyk <vosmerkin.evgen@gmail.com>
2025-02-24 00:02:41 +0200

    added create finfall

--- src/main/java/org/yevhen/dao/Main.java
+++ src/main/java/org/yevhen/dao/Main.java
@@ -7,21 +7,38 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-       DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
-                                                            "customerdb",
-                                                                "sa",
-                                                                "postgres")  ;
-       try {
-           Connection connection = dcm.getConnection();
-           Statement statement = connection.createStatement();
-           ResultSet rs = statement.executeQuery("SELECT * FROM customer;");
-           while (rs.next()){
-               System.out.println(rs.getInt(1));
-           }
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost:5435",
+                "marketplace",
+                "postgres",
+                "password");
+        try {
+            Connection connection = dcm.getConnection();
 
-       } catch (SQLException e) {
-           throw new RuntimeException(e);
-       }
+            create(connection);
+
+            getAll(connection);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
+
+    public static void create(Connection connection) {
+        CustomerDao customerDao = new CustomerDao(connection);
+        Customer customer = new Customer();
+        customer.setName("qwer");
+
+        customerDao.create(customer);
+    }
+
+    public static void getAll(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM customer;");
+//        ResultSet rs = statement.executeQuery("select * from information_schema.tables;");
+        while (rs.next()) {
+            System.out.println(rs.getInt(1));
+        }
+    }
+
 }
